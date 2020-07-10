0a6bf90e8d7339f14e28f4b9a3de8a5f60c03f18
atelierkarin <atelierkarin@gmail.com>
2020-07-10 20:26:52 +0800

    update nationality

--- nationality.py
+++ nationality.py
@@ -7,10 +7,13 @@ nationality[135] = 'KOR'
 nationality[141] = 'PHI'
 nationality[390] = 'USA'
 nationality[775] = 'ISR'
+nationality[787] = 'POL'
+nationality[796] = 'ESP'
 nationality[1435] = 'AUS'
 nationality[1649] = 'ARG'
 nationality[1650] = 'BOL'
 nationality[1651] = 'BRA'
+nationality[1652] = 'CHI'
 
 def getNationality(nid):
   return nationality[nid] if nid in nationality else nid
